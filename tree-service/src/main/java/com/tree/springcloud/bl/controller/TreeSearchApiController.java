8f746939117ab1e40c6c9771fc6e4eb6f851ccbe
viacheslav.vahanov <viacheslav.vahanov@chisw.com>
2018-02-05 11:39:10 +0200

    stringlike: added config server

--- tree-service/src/main/java/com/tree/springcloud/bl/controller/TreeSearchApiController.java
+++ tree-service/src/main/java/com/tree/springcloud/bl/controller/TreeSearchApiController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 import static com.tree.springcloud.bl.controller.TreeSearchApiController.URI;
 
